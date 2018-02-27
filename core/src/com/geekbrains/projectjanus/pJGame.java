0f5e4b7cd2b70cf22b7debb65fbad26d9418029d
Naif <knifffe@yandex.ru>
2018-02-27 23:46:38 +0300

    прототип

--- core/src/com/geekbrains/projectjanus/pJGame.java
+++ core/src/com/geekbrains/projectjanus/pJGame.java
@@ -1,33 +1,56 @@
 package com.geekbrains.projectjanus;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class pJGame extends ApplicationAdapter {
+public class pJGame extends Game
+{
 	SpriteBatch batch;
-	Texture img;
-	
+//	Texture img;
+//
+//	@Override
+//	public void create () {
+//		batch = new SpriteBatch();
+//		img = new Texture("badlogic.jpg");
+//	}
+//
+//	@Override
+//	public void render () {
+//		Gdx.gl.glClearColor(1, 0, 0, 1);
+//		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+//		batch.begin();
+//		batch.draw(img, 0, 0);
+//		batch.end();
+//	}
+//
+//	@Override
+//	public void dispose () {
+//		batch.dispose();
+//		img.dispose();
+//	}
+
 	@Override
-	public void create () {
+	public void create() {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		ScreenManager.getInstance().init(this, batch);
+//		ScreenManager.getInstance().switchScreen(ScreenManager.ScreenType.MENU);
+		ScreenManager.getInstance().switchScreen(ScreenManager.ScreenType.GAME);
+	}
+
+
+	@Override
+	public void render() {
+		float dt = Gdx.graphics.getDeltaTime();
+		getScreen().render(dt);
 	}
 
 	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
+	public void dispose() {
 		batch.dispose();
-		img.dispose();
 	}
+
 }
